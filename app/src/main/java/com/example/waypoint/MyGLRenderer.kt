b05e104afbaa0f652fc8ebd4168d67728cced8e1
jajkor <play4rj@aol.com>
2024-09-18 18:56:03 -0400

    feat: add support for touch gestures
    Added swipe and pinch gestures that allow you to pan and zoom the object.

--- app/src/main/java/com/example/waypoint/MyGLRenderer.kt
+++ app/src/main/java/com/example/waypoint/MyGLRenderer.kt
@@ -3,14 +3,31 @@ package com.example.waypoint
 import android.content.Context
 import android.opengl.GLES30
 import android.opengl.GLSurfaceView
+import android.opengl.Matrix
 import javax.microedition.khronos.egl.EGLConfig
 import javax.microedition.khronos.opengles.GL10
 
 // Controls what gets drawn on the GLSurfaceView it is associated with
 class MyGLRenderer(private val context: Context) : GLSurfaceView.Renderer {
     private var objModel: ObjModel? = null
-
     private lateinit var shaderProgram: ShaderProgram
+
+    private var cameraX: Float = 0.0f
+    private var cameraY: Float = 0.0f
+    private var cameraZ: Float = -5.0f
+    private var rotationX: Float = 0.0f
+    private var rotationY: Float = 0.0f
+    private var scaleFactor: Float = 1.0f
+
+    private lateinit var viewMatrix: FloatArray
+    private lateinit var projectionMatrix: FloatArray
+    private lateinit var modelMatrix: FloatArray
+
+    init {
+        viewMatrix = FloatArray(16)
+        projectionMatrix = FloatArray(16)
+        modelMatrix = FloatArray(16)
+    }
 
     // Called once to set up the view's OpenGL ES environment
     override fun onSurfaceCreated(unused: GL10, config: EGLConfig) {
@@ -25,14 +42,40 @@ class MyGLRenderer(private val context: Context) : GLSurfaceView.Renderer {
     override fun onDrawFrame(unused: GL10?) {
         GLES30.glUseProgram(shaderProgram.getProgram()) // Add program to OpenGL ES environment
 
-        GLES30.glClearColor(0.1f, 0.1f, 0.1f, 1.0f)
+        GLES30.glClearColor(0.0f, 0.0f, 0.0f, 1.0f)
         GLES30.glClear(GLES30.GL_COLOR_BUFFER_BIT or GLES30.GL_DEPTH_BUFFER_BIT)
 
-        objModel?.render()
+        Matrix.setLookAtM(viewMatrix, 0, cameraX, cameraY, cameraZ, 0f, 0f, 0f, 0f, 1f, 0f)
+
+        Matrix.setIdentityM(modelMatrix, 0)
+        Matrix.rotateM(modelMatrix, 0, rotationX, 1.0f, 0.0f, 0.0f)
+        Matrix.rotateM(modelMatrix, 0, rotationY, 0.0f, 1.0f, 0.0f)
+        Matrix.scaleM(modelMatrix, 0, scaleFactor, scaleFactor, scaleFactor)
+
+        val mvpMatrix = FloatArray(16)
+        Matrix.multiplyMM(mvpMatrix, 0, projectionMatrix, 0, viewMatrix, 0)
+        Matrix.multiplyMM(mvpMatrix, 0, mvpMatrix, 0, modelMatrix, 0)
+
+        val uMVPMatrixLocation = GLES30.glGetUniformLocation(shaderProgram.getProgram(), "u_MVPMatrix")
+        GLES30.glUniformMatrix4fv(uMVPMatrixLocation, 1, false, mvpMatrix, 0)
+
+        objModel?.render(shaderProgram, mvpMatrix)
     }
 
     override fun onSurfaceChanged(unused: GL10?, width: Int, height: Int) {
         GLES30.glViewport(0, 0, width, height)
+
+        val ratio: Float = width.toFloat() / height.toFloat()
+        Matrix.frustumM(projectionMatrix, 0, -ratio, ratio, -1f, 1f, 3f,7f)
+    }
+
+    fun onScroll(distanceX: Float, distanceY: Float) {
+        rotationX -= distanceY / 10.0f
+        rotationY -= distanceX / 10.0f
+    }
+
+    fun onScale(scaleFactor: Float) {
+        this.scaleFactor *= scaleFactor
     }
 }
 
--- app/src/main/java/com/example/waypoint/MyGLSurfaceView.kt
+++ app/src/main/java/com/example/waypoint/MyGLSurfaceView.kt
@@ -1,11 +1,16 @@
 package com.example.waypoint
 
 import android.content.Context
+import android.icu.number.Scale
 import android.opengl.GLSurfaceView
+import android.view.GestureDetector
 import android.view.MotionEvent
+import android.view.ScaleGestureDetector
 
 class MyGLSurfaceView(context: Context) : GLSurfaceView(context) {
     private val renderer: MyGLRenderer
+    private val gestureDetector: GestureDetector
+    private val scaleGestureDetector: ScaleGestureDetector
 
     init {
         // Create an OpenGL ES 3.0 context
@@ -15,5 +20,33 @@ class MyGLSurfaceView(context: Context) : GLSurfaceView(context) {
         renderer = MyGLRenderer(context)
         setRenderer(renderer)
         //renderMode = GLSurfaceView.RENDERMODE_WHEN_DIRTY
+
+        gestureDetector = GestureDetector(context, GestureListener())
+        scaleGestureDetector = ScaleGestureDetector(context, ScaleListener())
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        gestureDetector.onTouchEvent(event)
+        scaleGestureDetector.onTouchEvent(event)
+        return true
+    }
+
+    private inner class GestureListener : GestureDetector.SimpleOnGestureListener() {
+        override fun onScroll(
+            e1: MotionEvent?,
+            e2: MotionEvent,
+            distanceX: Float,
+            distanceY: Float
+        ): Boolean {
+            renderer.onScroll(distanceX, distanceY)
+            return true
+        }
+    }
+
+    private inner class ScaleListener() : ScaleGestureDetector.SimpleOnScaleGestureListener() {
+        override fun onScale(detector: ScaleGestureDetector) : Boolean {
+            renderer.onScale(detector.scaleFactor)
+            return true
+        }
     }
 }
