35aaafe0be0f0d68679956b7ff355d4d98934ea1
jajkor <play4rj@aol.com>
2024-09-18 19:04:41 -0400

    feat(shader): add fragment coloration based on vertex normal
    Changed fragment shader to color based on surface normal

--- app/src/main/res/raw/fragment_shader.glsl
+++ app/src/main/res/raw/fragment_shader.glsl
@@ -1,7 +1,10 @@
 #version 300 es
 precision mediump float;
+
+in vec3 Normal;
+
 out vec4 fragColor;
 
 void main() {
-    fragColor = vec4(1.0, 1.0, 1.0, 1.0);  // White color
+    fragColor = vec4(Normal, 1.0);  // White color
 }
--- app/src/main/res/raw/vertex_shader.glsl
+++ app/src/main/res/raw/vertex_shader.glsl
@@ -1,7 +1,12 @@
 #version 300 es
 layout(location = 0) in vec4 a_Position;
-uniform mat4 uMVPMatrix;
+layout(location = 1) in vec3 a_Normal;
+
+out vec3 Normal;
+
+uniform mat4 u_MVPMatrix;
 
 void main() {
-    gl_Position = a_Position;
+    Normal = a_Normal;
+    gl_Position = u_MVPMatrix * a_Position;
 }
