c62d4f5bf1cf48591890bc89c6b3899574745a9f
jajkor <play4rj@aol.com>
2024-10-03 19:36:52 -0400

    feat(renderer): added gooch shading
    Adding vertex and fragment shading using the Gooch Shading Model

--- app/src/main/res/raw/fragment_shader.glsl
+++ app/src/main/res/raw/fragment_shader.glsl
@@ -1,10 +1,50 @@
 #version 300 es
 precision mediump float;
 
+in vec3 FragPos;
 in vec3 Normal;
 
 out vec4 fragColor;
 
+uniform vec3 viewPos;
+uniform vec3 lightColor;
+uniform vec3 lightPos;
+
+uniform vec3 surfaceColor;
+uniform float diffuseWarm;
+uniform float diffuseCool;
+uniform vec3 warmColor;
+uniform vec3 coolColor;
+
 void main() {
-    fragColor = vec4(Normal, 1.0);  // White color
+    // Normalize the normal vector
+    vec3 norm = normalize(Normal);
+
+    // Calculate light direction
+    vec3 lightDir = normalize(lightPos - FragPos);
+
+    // Diffuse lighting
+    float NdotL = max(dot(norm, lightDir), 0.0);
+
+    // Gooch shading - interpolation between warm and cool colors
+    vec3 kCool = min(coolColor + diffuseCool * surfaceColor, 1.0f);
+    vec3 kWarm = max(warmColor + diffuseWarm * surfaceColor, 1.0f);
+    vec3 kFinal = mix(kCool, kWarm, NdotL);
+
+    // Specular highlights (Blinn-Phong)
+    float specularStrength = 0.5;
+    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 reflectDir = reflect(-lightDir, norm);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32.0);
+    vec3 specular = specularStrength * spec * lightColor;
+
+    // Combine Gooch shading and specular highlights with object color
+    //vec3 result = (goochColor + specular) * objectColor;
+
+    // Output the final fragment color
+    if (gl_FrontFacing) {
+        fragColor = vec4(min(kFinal + spec, 1.0), 1.0);
+    } else {
+        fragColor = vec4(0, 0, 0, 1);
+    }
 }
--- app/src/main/res/raw/vertex_shader.glsl
+++ app/src/main/res/raw/vertex_shader.glsl
@@ -1,12 +1,16 @@
 #version 300 es
-layout(location = 0) in vec4 a_Position;
+layout(location = 0) in vec3 a_Position;
 layout(location = 1) in vec3 a_Normal;
 
+out vec3 FragPos;
 out vec3 Normal;
 
-uniform mat4 u_MVPMatrix;
+uniform mat4 u_Model;
+uniform mat4 u_View;
+uniform mat4 u_Projection;
 
 void main() {
+    FragPos = vec3(u_Model * vec4(a_Position, 1.0));
     Normal = a_Normal;
-    gl_Position = u_MVPMatrix * a_Position;
+    gl_Position = u_Projection * u_View * u_Model * vec4(a_Position, 1.0);
 }
