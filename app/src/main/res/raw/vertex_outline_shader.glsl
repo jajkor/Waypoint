1a0737bdb383cafc54ac776eb3819357af27dc89
jajkor <play4rj@aol.com>
2024-10-04 11:51:11 -0400

    feat(renderer): add outline uniform parameter
    Added a uniform that can be passed to the outline fragment shader to change the scale of the outline

--- app/src/main/res/raw/vertex_outline_shader.glsl
+++ app/src/main/res/raw/vertex_outline_shader.glsl
@@ -5,6 +5,7 @@ layout(location = 1) in vec3 a_Normal;
 out vec3 FragPos;
 out vec3 Normal;
 
+uniform float u_Outline;
 uniform mat4 u_Model;
 uniform mat4 u_View;
 uniform mat4 u_Projection;
@@ -12,5 +13,5 @@ uniform mat4 u_Projection;
 void main() {
     FragPos = vec3(u_Model * vec4(a_Position, 1.0));
     Normal = a_Normal;
-    gl_Position = u_Projection * u_View * u_Model * vec4(a_Position + a_Normal * 0.0005, 1.0);
+    gl_Position = u_Projection * u_View * u_Model * vec4(a_Position + a_Normal * u_Outline, 1.0);
 }
