5faa6eb7ebad8016da75dd6d1f3998f154fb8d92
jajkor <play4rj@aol.com>
2024-10-10 21:48:49 -0400

    feat: added grid quad
    Added the a quad for an infinite grid

--- app/src/main/java/com/example/waypoint/MyGLRenderer.kt
+++ app/src/main/java/com/example/waypoint/MyGLRenderer.kt
@@ -1,29 +1,22 @@
 package com.example.waypoint
 
 import android.content.Context
-import android.opengl.GLES30
+import android.opengl.GLES30.*
 import android.opengl.GLSurfaceView
 import android.opengl.Matrix
-import java.nio.charset.Charset
 import javax.microedition.khronos.egl.EGLConfig
 import javax.microedition.khronos.opengles.GL10
+import javax.microedition.khronos.opengles.GL10.GL_MULTISAMPLE
 
-// Controls what gets drawn on the GLSurfaceView it is associated with
-class MyGLRenderer(private val context: Context) : GLSurfaceView.Renderer {
+class MyGLRenderer(private val context: Context, private val camera: Camera3D) : GLSurfaceView.Renderer {
     private var objModel: ObjModel? = null
     private var lightModel: ObjModel? = null
+    private var gridModel: ObjModel? = null
 
+    private lateinit var gridProgram: Program
     private lateinit var shaderProgram: Program
     private lateinit var outlineProgram: Program
     private lateinit var lightShaderProgram: Program
-
-    private var cameraX: Float = 0.0f
-    private var cameraY: Float = 0.0f
-    private var cameraZ: Float = -5.0f
-
-    private var rotationX: Float = 0.0f
-    private var rotationY: Float = 0.0f
-    private var scaleFactor: Float = 1.0f
 
     private var startTime = System.nanoTime()
 
@@ -39,83 +32,88 @@ class MyGLRenderer(private val context: Context) : GLSurfaceView.Renderer {
 
     // Called once to set up the view's OpenGL ES environment
     override fun onSurfaceCreated(unused: GL10, config: EGLConfig) {
-        GLES30.glClearColor(0.0f, 0.0f, 0.0f, 1.0f) // Set the background frame color
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f) // Set the background frame color
 
-        GLES30.glEnable(GLES30.GL_DEPTH_TEST)
-        GLES30.glDepthFunc(GLES30.GL_LESS);
-        GLES30.glEnable(GLES30.GL_STENCIL_TEST)
-        GLES30.glStencilFunc(GLES30.GL_NOTEQUAL, 1, 0xFF);
-        GLES30.glStencilOp(GLES30.GL_KEEP, GLES30.GL_KEEP, GLES30.GL_REPLACE) // Control result of passing/failing a test.
+        glEnable(GL_DEPTH_TEST)
+        glEnable(GL_BLEND)
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
 
-/*        shaderProgram = Program.create(
-            context.resources.readRawTextFile(R.raw.vertex_shader),
-            context.resources.readRawTextFile(R.raw.fragment_shader)
-        )
-        outlineProgram = Program.create(
-            context.resources.readRawTextFile(R.raw.fragment_outline_shader),
-            context.resources.readRawTextFile(R.raw.vertex_outline_shader)
-        )
-        objModel = ModelLoader(context).loadModel("walls_fixed.obj")
+        glDepthFunc(GL_LESS)
+        glEnable(GL_STENCIL_TEST)
+        glStencilFunc(GL_NOTEQUAL, 1, 0xFF);
+        glStencilOp(GL_KEEP, GL_KEEP, GL_REPLACE) // Control result of passing/failing a test.
 
-        lightShaderProgram = Program.create(
-            context.resources.readRawTextFile(R.raw.light_vertex_shader),
-            context.resources.readRawTextFile(R.raw.light_fragment_shader)
+        gridProgram = Program(
+            context.resources.readRawTextFile(R.raw.grid_vert),
+            context.resources.readRawTextFile(R.raw.grid_frag)
         )
-        lightModel = ModelLoader(context).loadModel("light_cube.obj")*/
+        gridModel = ModelLoader(context).loadModel("grid/quad.obj")
+
         shaderProgram = Program(
-            context.resources.openRawResource(R.raw.vertex_shader).readBytes().toString(Charset.defaultCharset()),
-            context.resources.openRawResource(R.raw.fragment_shader).readBytes().toString(Charset.defaultCharset())
+            context.resources.readRawTextFile(R.raw.campus_vert),
+            context.resources.readRawTextFile(R.raw.campus_frag)
         )
         outlineProgram = Program(
-            context.resources.openRawResource(R.raw.vertex_outline_shader).readBytes().toString(Charset.defaultCharset()),
-            context.resources.openRawResource(R.raw.fragment_outline_shader).readBytes().toString(Charset.defaultCharset())
+            context.resources.readRawTextFile(R.raw.outline_vert),
+            context.resources.readRawTextFile(R.raw.outline_frag)
         )
-        objModel = ModelLoader(context).loadModel("walls_fixed.obj")
+        objModel = ModelLoader(context).loadModel("campus/campus_walls.obj")
 
         lightShaderProgram = Program(
-            context.resources.openRawResource(R.raw.light_vertex_shader).readBytes().toString(Charset.defaultCharset()),
-            context.resources.openRawResource(R.raw.light_fragment_shader).readBytes().toString(Charset.defaultCharset())
+            context.resources.readRawTextFile(R.raw.light_vert),
+            context.resources.readRawTextFile(R.raw.light_frag)
         )
-        lightModel = ModelLoader(context).loadModel("light_cube.obj")
+        lightModel = ModelLoader(context).loadModel("light/light_cube.obj")
     }
 
     // Called for each redraw of the view
     override fun onDrawFrame(unused: GL10?) {
-        GLES30.glEnable(GLES30.GL_DEPTH_TEST)
-        GLES30.glEnable(GLES30.GL_STENCIL_TEST)
-        GLES30.glStencilOp(GLES30.GL_KEEP, GLES30.GL_KEEP, GLES30.GL_REPLACE);
+        glEnable(GL_MULTISAMPLE);
+        glEnable(GL_DEPTH_TEST)
+        glEnable(GL_BLEND)
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+        glEnable(GL_STENCIL_TEST)
+        glStencilOp(GL_KEEP, GL_KEEP, GL_REPLACE)
 
-        GLES30.glClearColor(112/255f, 128/255f, 144/255f, 1.0f)
-        GLES30.glClear(GLES30.GL_COLOR_BUFFER_BIT or GLES30.GL_DEPTH_BUFFER_BIT or GLES30.GL_STENCIL_BUFFER_BIT)
+        //glClearColor(0.0f, 0.0f, 0.0f, 0.0f)
+        glClearColor(112/255f, 128/255f, 144/255f, 1.0f)
+        glClear(GL_COLOR_BUFFER_BIT or GL_DEPTH_BUFFER_BIT or GL_STENCIL_BUFFER_BIT)
 
-        Matrix.setLookAtM(viewMatrix, 0, cameraX, cameraY, cameraZ, 0f, 0f, 0f, 0f, 1f, 0f)
+        viewMatrix = camera.getViewMatrix()
 
         val elapsedTime = (System.nanoTime() - startTime) / 1_000_000_000.0f
-        val lightX = Math.sin(elapsedTime.toDouble()).toFloat() * 20f // Move along the x-axis
-        val lightY = 20.0f
-        val lightZ = Math.cos(elapsedTime.toDouble()).toFloat() * 20f// Move along the z-axis
+        val lightPosition = Vector3(
+            Math.sin(elapsedTime.toDouble()).toFloat() * 20f,
+            20.0f,
+            Math.cos(elapsedTime.toDouble()).toFloat() * 20f
+        )
 
-        GLES30.glStencilMask(0xFF)
+        //camera.setPivot(Vector3(lightPosition.x, 0.0f, lightPosition.z))
+
+        glStencilMask(0x00)
+        gridProgram.use()
+        Matrix.setIdentityM(modelMatrix, 0)
+        gridProgram.setMat4("u_Model", modelMatrix)
+        gridProgram.setMat4("u_View", viewMatrix)
+        gridProgram.setMat4("u_Projection", projectionMatrix)
+        gridModel?.render()
+
+        glStencilMask(0xFF)
         lightShaderProgram.use()
         Matrix.setIdentityM(modelMatrix, 0)
         Matrix.scaleM(modelMatrix, 0, 0.2f, 0.2f, 0.2f)
-        Matrix.scaleM(modelMatrix, 0, scaleFactor, scaleFactor, scaleFactor)
-        Matrix.rotateM(modelMatrix, 0, rotationX, 1.0f, 0.0f, 0.0f)
-        Matrix.rotateM(modelMatrix, 0, rotationY, 0.0f, 1.0f, 0.0f)
-        Matrix.translateM(modelMatrix, 0, lightX, lightY, lightZ)
+        Matrix.translateM(modelMatrix, 0, lightPosition.x, lightPosition.y, lightPosition.z)
         lightShaderProgram.setMat4("u_Model", modelMatrix)
         lightShaderProgram.setMat4("u_View", viewMatrix)
         lightShaderProgram.setMat4("u_Projection", projectionMatrix)
-        lightModel?.render(lightShaderProgram, modelMatrix)
+        lightModel?.render()
 
         // 1st. render pass: Render the object and update the stencil buffer
-        GLES30.glStencilFunc(GLES30.GL_ALWAYS, 1, 0xFF)  // Always pass the stencil test
-        GLES30.glStencilMask(0xFF)  // Enable writing to stencil buffer
+        glStencilFunc(GL_ALWAYS, 1, 0xFF)  // Always pass the stencil test
+        glStencilMask(0xFF)  // Enable writing to stencil buffer
         shaderProgram.use()
         Matrix.setIdentityM(modelMatrix, 0)
-        Matrix.scaleM(modelMatrix, 0, scaleFactor, scaleFactor, scaleFactor)
-        Matrix.rotateM(modelMatrix, 0, rotationX, 1.0f, 0.0f, 0.0f)
-        Matrix.rotateM(modelMatrix, 0, rotationY, 0.0f, 1.0f, 0.0f)
+        //Matrix.translateM(modelMatrix, 0, 0.0f, 1.25f, 0.0f)
         shaderProgram.setMat4("u_Model", modelMatrix)
         shaderProgram.setMat4("u_View", viewMatrix)
         shaderProgram.setMat4("u_Projection", projectionMatrix)
@@ -125,61 +123,33 @@ class MyGLRenderer(private val context: Context) : GLSurfaceView.Renderer {
         shaderProgram.setFloat("diffuseCool", 0.3f)
         shaderProgram.setFloat3("warmColor", Vector3(255/255f, 204/255f, 153/255f))
         shaderProgram.setFloat3("coolColor", Vector3(0.0f, 0.0f, 0.6f))
-        shaderProgram.setFloat3("lightPos", Vector3(lightX, lightY, lightZ))
-        val cameraPosition = getCameraPositionInWorldSpace(viewMatrix)
-        shaderProgram.setFloat3("viewPos", Vector3(cameraPosition[0], cameraPosition[1], cameraPosition[2]))
-        objModel?.render(shaderProgram, modelMatrix)
+        shaderProgram.setFloat3("lightPos", lightPosition)
+        shaderProgram.setFloat3("viewPos", camera.position)
+        objModel?.render()
 
         // Second Pass: Render outline, only where stencil is not equal to 1 (outside the cube)
-        GLES30.glStencilFunc(GLES30.GL_NOTEQUAL, 1, 0xFF)  // Only pass where stencil value is not 1
-        GLES30.glStencilMask(0x00)  // Disable writing to the stencil buffer
-        GLES30.glDisable(GLES30.GL_DEPTH_TEST)  // Disable depth testing for the outline
+        glStencilFunc(GL_NOTEQUAL, 1, 0xFF)  // Only pass where stencil value is not 1
+        glStencilMask(0x00)  // Disable writing to the stencil buffer
+        glDisable(GL_DEPTH_TEST)  // Disable depth testing for the outline
         outlineProgram.use()
         Matrix.setIdentityM(modelMatrix, 0)
-        Matrix.scaleM(modelMatrix, 0, scaleFactor, scaleFactor, scaleFactor)
-        Matrix.rotateM(modelMatrix, 0, rotationX, 1.0f, 0.0f, 0.0f)
-        Matrix.rotateM(modelMatrix, 0, rotationY, 0.0f, 1.0f, 0.0f)
+        //Matrix.translateM(modelMatrix, 0, 0.0f, 1.25f, 0.0f)
         outlineProgram.setMat4("u_Model", modelMatrix)
         outlineProgram.setMat4("u_View", viewMatrix)
         outlineProgram.setMat4("u_Projection", projectionMatrix)
         outlineProgram.setFloat("u_Outline", 0.05f)
-        objModel?.render(outlineProgram, modelMatrix)
+        objModel?.render()
 
         // Reset stencil and depth test states
-        GLES30.glStencilMask(0xFF)  // Re-enable stencil writing
-        GLES30.glStencilFunc(GLES30.GL_ALWAYS, 1, 0xFF)  // Reset stencil function
-        GLES30.glEnable(GLES30.GL_DEPTH_TEST)  // Re-enable depth test
-    }
-
-    fun getCameraPositionInWorldSpace(viewMatrix: FloatArray): FloatArray {
-        val inverseViewMatrix = FloatArray(16)
-
-        // Calculate the inverse of the view matrix
-        Matrix.invertM(inverseViewMatrix, 0, viewMatrix, 0)
-
-        // Extract the camera position from the inverse view matrix (last column)
-        return floatArrayOf(
-            inverseViewMatrix[12], // x
-            inverseViewMatrix[13], // y
-            inverseViewMatrix[14]  // z
-        )
+        glStencilMask(0xFF)  // Re-enable stencil writing
+        glStencilFunc(GL_ALWAYS, 1, 0xFF)  // Reset stencil function
+        glEnable(GL_DEPTH_TEST)  // Re-enable depth test
     }
 
     override fun onSurfaceChanged(unused: GL10?, width: Int, height: Int) {
-        GLES30.glViewport(0, 0, width, height)
+        glViewport(0, 0, width, height)
 
         val ratio: Float = width.toFloat() / height.toFloat()
-        Matrix.frustumM(projectionMatrix, 0, -ratio, ratio, -1f, 1f, 3f,10f)
-    }
-
-    fun onScroll(distanceX: Float, distanceY: Float) {
-        rotationX -= distanceY / 10.0f
-        rotationY -= distanceX / 10.0f
-    }
-
-    fun onScale(scaleFactor: Float) {
-        this.scaleFactor *= scaleFactor
+        Matrix.frustumM(projectionMatrix, 0, -ratio, ratio, -1f, 1f, 3.0f,100f)
     }
 }
-
-
